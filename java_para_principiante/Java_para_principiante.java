18915569aa0b4e7d979d7d2508fb899a98036dc1
Anibal <stockmetal118@hotmail.com>
2014-03-27 19:59:25 -0600

    Aquí agregué el proyecto a netbeans

---
+++ java_para_principiante/Java_para_principiante.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_para_principiante;
+
+/**
+ *
+ * @author Anibal
+ */
+public class Java_para_principiante {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
